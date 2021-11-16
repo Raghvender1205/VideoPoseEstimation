9c00cf397b958b8440f0ae650babaf1f4697db87
Raghvender <raghvender1205@gmail.com>
2021-11-16 13:32:09 +0530

    Changed Some things

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 mpDraw = mp.solutions.drawing_utils
 
-#cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture('videos/b.mp4')
 pTime = 0
 
@@ -29,6 +29,6 @@ while True:
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (50,50), cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0), 3)
+    cv2.putText(img, str(int(fps)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
